d4377dd5b8567d6b377a92ed2e22c59fd23091ef
Lerot <alsrltt@nate.com>
2018-04-28 16:01:28 +0900

    db find, create기능

--- server/app.js
+++ server/app.js
@@ -3,11 +3,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var Users = require('./models/users');
 
 var app = express();
+
+//mongod connection
+var db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', function(){
+  console.log("Connected to mongod server");
+});
+mongoose.connect('mongodb://localhost:27017/tuttonote');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,6 +29,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,34 @@
 var express = require('express');
+var Users = require('../models/users');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  Users.find(function(err, userlist){
+    if(err){
+      console.error(err);
+      res.json({result : 0});
+      return;
+    }
+    res.json(userlist);
+  });
 });
+
+router.post('/', function(req, res){
+  var users = new Users();
+  users.userid = req.query.userid;
+  users.name = req.query.name;
+  users.password = req.query.password;
+
+  users.save(function(err){
+    if(err){
+      console.error(err);
+      res.json({result: 0});
+      return;
+    }
+    res.json({result: 1});
+  });
+
+});
+
 
 module.exports = router;
