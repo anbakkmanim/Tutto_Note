4f03089404c12834f27f76f9ad7b02152f81b75a
J1NC <womin1120@naver.com>
2018-04-28 14:55:10 +0900

    users.js 수정

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:_id', function(req, res, next){
+  console.log(req.params._id);
+  if(req.params._id === '1234'){
+    res.send({ status: 1, userid: 'AAAA', name: 'ㅁㄴㅇ'});
+  }else{
+    res.send({ status: 0 });
+  }
 });
+
+
 
 module.exports = router;
