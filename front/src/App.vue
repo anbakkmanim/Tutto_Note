4490e3a010607988e35e3c79d5f61d227bb2b590
남현욱 <37534735+hw0k@users.noreply.github.com>
2018-05-12 16:31:52 +0900

    .gitignore Re-defined

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,12 +1,8 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
     <!--<img src="./assets/logo.png">
     <!<router-view/>-->
     <user />
-=======
-    <h1>{{msg}}</h1>
->>>>>>> 829da4df431d9c66e74cac16bf6be243702b9324
   </div>
 </template>
 
@@ -16,23 +12,14 @@ import home from '@/components/home'
 
 export default {
   name: 'App',
-<<<<<<< HEAD
   components: {
    'user': home,
   },
   
-=======
-  data () {
-    return {
-      msg: 'UMin'
-    }
-  }
->>>>>>> 829da4df431d9c66e74cac16bf6be243702b9324
 }
 </script>
 
 <style>
-<<<<<<< HEAD
 /*#app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -44,7 +31,4 @@ export default {
 .menu{
   
 }
-=======
-
->>>>>>> 829da4df431d9c66e74cac16bf6be243702b9324
 </style>
