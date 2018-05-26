38e2e2103b282af8d7c336ef36be0561838c921f
남현욱 <37534735+hw0k@users.noreply.github.com>
2018-05-26 15:53:45 +0900

    VueSession / Materialize.css 사용

--- front/src/main.js
+++ front/src/main.js
@@ -2,7 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 
 // 모듈 임포트
+
+// axios
 import axios from 'axios'
+// vue-session
+import VueSession from 'vue-session'
+// Materialize
+import "./css/materialize.scss"
+import "./js/bin/materialize.min.js"
 
 // 라우터
 import router from './router'
@@ -11,6 +18,7 @@ Vue.config.productionTip = false;
 
 // axios 사용
 Vue.prototype.$http = axios;
+Vue.use(VueSession);
 
 // Vue 객체 정의
 new Vue({
