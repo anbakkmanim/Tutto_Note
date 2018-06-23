8b6423447db696d63c015eb173b2cfeef8c29fe3
SweetRain16 <37534993+SweetRain16@users.noreply.github.com>
2018-06-23 16:10:13 +0900

    태그검색

--- front/src/components/Child3.vue
+++ front/src/components/Child3.vue
@@ -15,21 +15,54 @@
     <br>
     <router-link to="/child2">To Child2 Component</router-link>
     <br>
+    <ul class="tags">
+      <li v-for="(tag, index) in tags" :key="index">
+        {{ tag }}
+        <a v-on:click="remove(index)">x</a>
+      </li>
+    </ul>
+    <input type="text" v-model="input">
+    <button v-on:click="add">태그추가</button>
     <router-link to="/">To Home</router-link>
+    
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'Child3',
   data() {
+  
     return {
-
+      tags: [
+        'hi',
+        'hello',
+        'haha',
+        'hihi'
+      ],
+      input: ""
+    }
+  },
+  methods: {
+    add() {
+      this.tags.push(this.input);
+      this.input = "";
+    },
+    remove(index) {
+      this.tags.splice(index, 1);
     }
   }
 }
 </script>
 
 <style lang="scss">
-
+.tags {
+  li {
+    display: inline-block;
+    padding: 7px;
+    margin-right: 3px;
+    background-color: #eee;
+  }
+}
 </style>
